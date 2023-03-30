c034a7d2cef606cdc3880756a16c2da82b0385d2
marcos-baia <marcosjsbaia@yahoo.com.br>
2023-03-30 19:43:27 -0300

    establishing connection

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> responde = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+        
+        // extrair só os dados que interessam (titulo, poster, classificação)
+
+        // exibir e manipular os dados
     }
 }
