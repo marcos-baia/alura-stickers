edcb39ff75abd70aa96f0445c2e451ade620ac52
marcos-baia <marcosjsbaia@yahoo.com.br>
2023-03-30 22:10:16 -0300

    finalizing the project

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
         // fazer uma conexão HTTP e buscar os top 250 filmes
@@ -12,10 +14,17 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> responde = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
         
         // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(json) body;
 
         // exibir e manipular os dados
+        for(Map<String, String> filme: listaDeFilmes){
+            System.out.println(filme.get(key:"title"));
+            System.out.println(filme.get(key:"image"));
+            System.out.println(filme.get(key:"imDbRating"));
+            System.out.println();
+        }
     }
 }
